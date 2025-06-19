417eeae82432ef8a1ba183cd350b73f556e0897e
JosepoMP <jomipahi2006@gmail.com>
2025-06-19 10:27:05 -0500

    creacion boton de validacion

---
+++ button_Validation.js
@@ -0,0 +1,23 @@
+// declaramos una funcion que se ejecutaal hacer clic en el boton
+
+function validarEntrada() {
+  // obtenemos el valor del input
+  let valorIngresado = document.getElementById("entrada").value;
+
+  // validamos si el valor esta vacio (falsy) o tiene contenido (truthy)
+  if (valorIngresado) {
+    // si tiene texto, mostramos un mensaje positivo
+
+    document.getElementById("mensaje").textContent =
+      "¡Gracias por escribir algo!";
+
+    document.getElementById("mensaje").style.color = "green";
+  } else {
+    // si esta vacio, mostramosun mensaje de advertencia
+
+    document.getElementById("mensaje").textContent =
+      "Elcampo esta vacio, por favor escribe algo";
+
+    document.getElementById("mensaje").style.color = "red";
+  }
+}
